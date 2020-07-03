e7f6adae1ddef61bc0e450230a471f5f8c5eede0
eren.celik <eren.celik@aegon.com.tr>
2020-07-03 23:21:38 +0300

    client and config implemented

---
+++ client-service/src/main/java/com/eren/client/controller/ClientController.java
@@ -0,0 +1,19 @@
+package com.eren.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ClientController {
+	
+	@Value("${value}")
+	private String valueFromConfigFile;
+	
+	@GetMapping(value = "/value")
+	public String getValue() {
+		return valueFromConfigFile;
+	}
+
+}
